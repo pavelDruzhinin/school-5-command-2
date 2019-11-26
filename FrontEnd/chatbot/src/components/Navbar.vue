75f8ad40d09644c30ed0963b724513b7d31ab685
alxwtf <alxwtf@gmail.com>
2019-11-26 23:25:15 +0300

    Navbar test

---
+++ FrontEnd/chatbot/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <b-navbar toggleable="md" type="dark" variant="dark" sticky>
+        <b-navbar-brand to="/">Navbar test</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"/>
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item to="/">Home</b-nav-item>
+                <b-nav-item to="about">About</b-nav-item>
+            </b-navbar-nav>
+            <b-navbar-nav class="ml-auto">
+                <b-nav-form>
+                  <!-- <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input> -->
+                  <b-button class="my-2 my-sm-0 mr-sm-2">Log In</b-button>
+                  <b-button class="my-2 my-sm-0">Register</b-button>
+                </b-nav-form>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
