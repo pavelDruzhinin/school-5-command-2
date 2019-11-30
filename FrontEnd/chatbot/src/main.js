16ba2e7d0c11842da0c86238f8ff9261fe88934a
iquink <iquink@gmail.com>
2019-11-30 16:20:31 +0300

    default main.js, можно встраивать компонент chatsList

--- FrontEnd/chatbot/src/main.js
+++ FrontEnd/chatbot/src/main.js
@@ -3,12 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import css from './main.scss'
-import chatsList from './chatsList.vue'
+
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(chatsList)
+  render: h => h(App)
 }).$mount('#app')
