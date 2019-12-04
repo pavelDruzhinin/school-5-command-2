cfbbfcd80b32cef0ee0019e81d2867ca86622eec
alxwtf <alxwtf@gmail.com>
2019-12-05 00:45:47 +0300

    Backend register, frontend changes

--- FrontEnd/chatbot/src/store/index.js
+++ FrontEnd/chatbot/src/store/index.js
@@ -10,7 +10,7 @@ export default new Vuex.Store({
     }
   },
   getters: {
-    isAuthenticated: state => state.profile.email
+    isAuthenticated: state => state.profile.email && state.profile.fullname
   },
   mutations: {
     setProfile(state, profile) {
