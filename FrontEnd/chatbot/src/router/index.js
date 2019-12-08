f23c8c6aec3872052cb7fdcd52f23975e8a94309
alxwtf <alxwtf@gmail.com>
2019-12-08 21:26:23 +0300

    merge

--- FrontEnd/chatbot/src/router/index.js
+++ FrontEnd/chatbot/src/router/index.js
@@ -18,8 +18,8 @@ const routes = [
     path: '/',
     name: 'Home',
     components: {
-      default: () => import('@/views/Home.vue'),
-      navbar: () => import('@/components/Navbar.vue')
+      default: () => import('../views/Home.vue'),
+      navbar: () => import('../components/Navbar.vue')
     }
   },
   {
@@ -41,19 +41,19 @@ const routes = [
     path: '/auth',
     beforeEnter:ifAuth,
     components: {
-      default: () => import('@/views/Auth/Index.vue'),
-      navbar: () => import('@/components/Navbar.vue')
+      default: () => import('../views/Auth/Index.vue'),
+      navbar: () => import('../components/Navbar.vue')
     },
     children: [
       {
         path: 'registration',
         name: 'Registration',
-        component: () => import('@/views/Auth/Registration.vue')
+        component: () => import('../views/Auth/Registration.vue')
       },
       {
         path: 'login',
         name: 'Login',
-        component: () => import('@/views/Auth/Login.vue')
+        component: () => import('../views/Auth/Login.vue')
       }
     ]
   }
